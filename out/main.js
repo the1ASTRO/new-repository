032b48401e24c074504a2a615465c117bce3d911
the1ASTRO <abulovshoxruh8@gmail.com>
2022-03-03 19:03:27 +0500

    masala ishladim

---
+++ out/main.js
@@ -0,0 +1,4 @@
+"use strict";
+// let x: number = -45
+// let y: number = 0
+Object.defineProperty(exports, "__esModule", { value: true });
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,78 @@
+// let x: number = -45
+// let y: number = 0
 
-console.log("Salom Shoxrux");
+// if (x > 0) {
+//     y = Math.cos(x)
+//     console.log("y=cos:" , y);
+// }
+// else {
+//     y = Math.sin(x)
+//     console.log("y=sin" , y);
+    
+// }
+
+// let a: number = 5
+// let b: number = 6
+
+// if (a > b) {
+//     console.log("a katta");
+    
+// }
+// else {
+//     console.log("b katta");
+    
+// }
+
+// let km: number = 1.5
+// let ft: number = 40
+ 
+// if (km * 1000 > ft * 0.305) {
+//     console.log("km katta");
+    
+// }
+// else {
+//     console.log("ft katta");
+    
+// }
+// let kmst: number = 5
+// let ms: number = 4
+
+// if (ms * 3.6 > kmst ){
+//     console.log("ms katta");
+// }
+// else{
+//     console.log("kmst katta");
+    
+// }
+
+// let rad: number = 5
+// let kv: number = 5
+
+// if (3.14 * rad * rad > kv * kv){
+//     console.log("rad katta");
+// }
+// else {
+//     console.log("kv katta");
+// }
+
+// let kv: number = 4
+// let tt1: number = 6
+// let tt2: number = 4
+
+// if (tt1 > kv && tt2 > kv){
+//     console.log("tt kv ichida");
+    
+// }
+// else {
+//     console.log("tt kv ichida");
+    
+// }
+// let n: number = 0
+// if (-5 < n && n < 3){
+//     console.log("oraligida");
+// }
+// else {
+//     console.log("oraligida emas");
+    
+// }
+export {}
